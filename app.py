1ef3ff42ddf04a9a2e17f0e05a1da2568edb5060
Roberto Rossi <robros@gmail.com>
2024-11-07 21:13:22 +0000

    Added back Response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response, jsonify
 from flask_cors import CORS # https://github.com/corydolphin/flask-cors
 
 from typing import List
